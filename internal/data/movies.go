aa7603c823d18a9d968d3ad3bc4fd1d6682acfd7
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-01-05 20:23:53 +0100

    add endpoint to delete movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -131,6 +131,31 @@ func (m MovieModel) Update(movie *Movie) error {
 }
 
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+	DELETE FROM movies
+	WHERE id = $1
+	`
+
+	result, err := m.DB.Exec(query)
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
 
